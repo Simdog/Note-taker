8371e80b2dea992b62eb8cd00e84094d4918ae2f
Simdog <oguny006@umn.edu>
2020-06-16 23:28:48 -0500

    got the note takers server up and half functioning, delete function next

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const util = require("util");
+const readAsync = util.promisify(fs.readFile);
+const writeAsync = util.promisify(fs.writeFile);
+
 
 const app = express();
 const PORT = process.env.PORT || 7500;
 
 app.use(express.urlencoded ({ extended: true }));
 app.use (express.json());
+app.use(express.static("public"));
 
 
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+    console.log("the notes is working");
+});
+
+app.get("/api/notes", async (req, res) => {
+    try {
+        return res.json(JSON.parse(await readAsync(path.join(__dirname, "/db/db.json"), 'utf-8')));
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+app.post("/api/notes", async (req, res) => {
+try {
+    let newNote = JSON.parse(JSON.stringify(req.body));
+    newNote.id = uuidv4();
+    console.log(newNote);
+
+    let newArray = JSON.parse(await readAsync(path.join(__dirname, "/db/db.json"), 'utf-8'));
+
+    newArray.push(newNote);
+    await writeAsync(path.join(__dirname, "/db/db.json"), JSON.stringify(newArray));
+
+    return res.json(newNote);
+
+} catch (err) {
+console.log(err);
+}
+});
+app.delete("")
+
+app.listen(PORT, function (){ 
+    console.log("App listening to PORT:" + PORT);
+
+});
